a0cf79f3506dfc41c4e16ea1204ee21bcfe7be6b
AHA <adam.halas@databyte.pl>
2021-06-13 10:48:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FC_control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
